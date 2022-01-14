710c46e472b6710fc8becf0754f0ad1e4f693c0d
Pikuman83 <wahab_anjum@hotmail.com>
2022-01-14 22:47:00 +0100

    Quick playground setup to test functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'daterangepicker';
+  title = 'unblur Demo';
+  isOPen = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
